e14666bd11f32fdc3d0c36b58d3a561ed08d027a
Oluwashola001 <oyagbinrinoluwashola@gmail.com>
2022-09-09 19:05:50 -0700

    first C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
-1 #include <stdio.h>
-2
-3 /**
-4    * main - Entry point
-5    * Description: prints "\"Programming is like building a multilingual puzzle"
-6    *  Return: 0
-7    */
-8
-9 int main(void)
-{
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
